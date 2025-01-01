98a5cd8f4513716c74bc5fc5520bd105343a434a
Jinesh87 <zodiacaus@gmail.com>
2025-01-01 17:46:01 +0530

    Managing Form STate and Submission

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { StyleSheet, View, Text } from "react-native";
 import Input from './Input';
+import Button from "../UI/Button";
 
-function ExpenseForm() {
+function ExpenseForm({ submitButtonLabel, onCancel, onSubmit }) {
     const [inputValues, setInputValues] = useState({
         amount: '',
         date: '',
@@ -15,6 +16,16 @@ function ExpenseForm() {
                 [inputIdentifier]: enteredValue,
             };
         });
+    }
+
+    function submitHandler() {
+        const expenseData = {
+            amount: +inputValues.amount,
+            date: new Date(inputValues.date),
+            description: inputValues.description
+        };
+
+        onSubmit(expenseData);
     }
 
     return (
@@ -51,6 +62,14 @@ function ExpenseForm() {
                     value: inputValues.description,
                 }}
             />
+            <View style={styles.buttons}>
+                <Button style={styles.button} mode="flat" onPress={onCancel}>
+                    Cancel
+                </Button>
+                <Button style={styles.button} onPress={submitHandler}>
+                    {submitButtonLabel}
+                </Button>
+            </View>
         </View>
     );
 }
@@ -73,5 +92,14 @@ const styles = StyleSheet.create({
     },
     rowInput: {
         flex: 1,
+    },
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    button: {
+        minWidth: 120,
+        marginHorizontal: 8,
     },
 });
--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -2,7 +2,7 @@ import { useContext, useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
-import Button from "../components/UI/Button";
+
 import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
@@ -41,14 +41,13 @@ function ManageExpense({ route, navigation }) {
         navigation.goBack();
     }
     return (
-        <View style={styles.container}>  
-        <ExpenseForm />          
-            <View style={styles.buttons}>
-                <Button style={styles.button} mode="flat" onPress={cancelHandler}>
-                    Cancel
-                </Button>
-                <Button style={styles.button} onPress={confirmHandler}>{isEditing ? "Update" : "Add"}</Button>
-            </View>
+        <View style={styles.container}>
+            <ExpenseForm
+                submitButtonLabel={isEditing ? 'Update' : 'Add'}
+                onSubmit={confirmHandler}
+                onCancel={cancelHandler}
+            />
+
             {isEditing && (
                 <View style={styles.deleteContainer}>
                     <IconButton
@@ -70,15 +69,6 @@ const styles = StyleSheet.create({
         flex: 1,
         padding: 24,
         backgroundColor: GlobalStyles.colors.primary800,
-    },
-    buttons: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    button: {
-        minWidth: 120,
-        marginHorizontal: 8,
     },
     deleteContainer: {
         marginTop: 16,
