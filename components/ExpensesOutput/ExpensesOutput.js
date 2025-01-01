8bf1713c2b371d292858385fa4a52b14b5a574e2
Jinesh87 <zodiacaus@gmail.com>
2025-01-01 16:08:56 +0530

    Fallback Text

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,24 +1,35 @@
-import { View, StyleSheet} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 import { GlobalStyles } from '../../constants/styles';
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
 
-function ExpensesOutput({ expenses, expensesPeriod }) {
-    return (
-      <View style={styles.container}>
-        <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-        <ExpensesList expenses={expenses} />
-      </View>
-    );
-  }
-  
-  export default ExpensesOutput;
-
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      padding: 24,
-      backgroundColor: GlobalStyles.colors.primary700
+function ExpensesOutput({ expenses, expensesPeriod, fallbackText }) {
+    let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+    if (expenses.length > 0) {
+        content = <ExpensesList expenses={expenses} />;
     }
-  });
+
+    return (
+        <View style={styles.container}>
+            <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
+            {content}
+        </View>
+    );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700
+    },
+    infoText: {
+        color: 'white',
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 32,
+    },
+});
